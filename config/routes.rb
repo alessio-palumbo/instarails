6632f86bb1b0e9bc88a7a02edc48936ad928e1ea
Alessio-Palumbo <alessio.palumbo4@gmail.com>
2017-09-27 14:47:32 +1000

    Add comments functionalities

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :photos
   root 'landing#index'
-
+  devise_for :users
+  resources :photos do
+    resources :comments #/comments
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
