43d83bdbf69102fe87d5e81544102dcd77e1d2fe
Alessio-Palumbo <alessio.palumbo4@gmail.com>
2017-09-28 10:24:05 +1000

    Add Messenger

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+
   root 'landing#index'
   devise_for :users
   resources :photos do
     resources :comments #/comments
+  end
+
+  resources :conversations do
+    resources :messages
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170927053826) do
+ActiveRecord::Schema.define(version: 20170927060941) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "photo_id"
@@ -23,11 +23,29 @@ ActiveRecord::Schema.define(version: 20170927053826) do
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
+  create_table "conversations", force: :cascade do |t|
+    t.integer "sender_id"
+    t.integer "recipient_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "likes", id: false, force: :cascade do |t|
     t.integer "photo_id", null: false
     t.integer "user_id", null: false
     t.datetime "created_at"
     t.index ["photo_id", "user_id"], name: "index_likes_on_photo_id_and_user_id", unique: true
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.integer "conversation_id"
+    t.integer "user_id"
+    t.boolean "read", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "photos", force: :cascade do |t|
